7054bab7bba0d3091766a7358fbf46034c7b7637
leseul-yj <79645550@qq.com>
2020-08-10 23:33:15 +0800

    forward官网

---
+++ website/src/i18n/en.js
@@ -0,0 +1,76 @@
+export default {
+    COMMON: {
+        MORE: "Learn More"
+    },
+    NAV: {
+        HOME: "Home",
+        TITLE: "Forward Chat",
+        ABOUT_US: "About",
+        LANGUAGE: "EN",
+        LANGUAGE_EN: "EN",
+        LANGUAGE_ZH: "CN",
+    },
+    HOME: {
+        SUMMARY: {
+            TITLE: "Without Distractions - Talk As You Want",
+            SUBTITLE: "Pay More Attention to Privacy * and Security*. Let’s start a free talk to say what you wanna say!",
+            ANDROID_DOWN: "Android Download",
+            IOS_DOWN: "IOS Download"
+        },
+        FEATURE: {
+            TITLE: "End-to-end encryption makes a safer chat software",
+            SUBTITLE: "Two-way Recall, No Storage in Cloud.",
+            TITLE_ONE: "End-to-end Encryption",
+            SUBTITLE_ONE: "We use end-to-end encryption technology to make you share your private information (images, voice messages, texts, videos, files, etc.) safely.",
+            TITLE_TWO: "Chat Mode",
+            SUBTITLE_TWO: "Single chat, group chat, voice chat & video chat. You can chat anonymously in group chat mode, expressing your true feelings and thoughts freely.",
+            TITLE_TREE: "Two-way Recall and Removal",
+            SUBTITLE_TREE: "Click “recall” and the chat records will be recalled in both devices, and all chat records on your device and the other party's device will be irretrievably cleared at the same time.",
+            TITLE_FOUR: "Destruction of the Password",
+            SUBTITLE_FOUR: "Enter “destruction of password” in the lock screen passwaord window to destroy the account and all information, and the APP can be formatted as it was originally downloaded."
+
+        },
+        SECTION_ONE: {
+            TITLE: "Security of Encrypted Chat",
+            SUBTITLE: "Whether you are in a single chat or a group chat, we will use end-to-end encryption technology to secure all content and files. Only you and the other party you communicate with can read or listen to the content sent, and no one else (including the APP) can read or listen to the content."
+        },
+        SECTION_TWO: {
+            TITLE: "Privacy of Chat Records",
+            SUBTITLE: "When you regret sending the message or do not want the information to be disseminated, you can use the “recall” function to withdraw it in both sides, and delete all the chat history on your and the other party's device at the same time, which can never be recovered. It’s safe and reliable."
+        },
+        SECTION_THREE: {
+            TITLE: "Leave You A Private Space",
+            SUBTITLE: "Every time you log in or change your device, we will change your private key at random, making your personal space more private and safe. In a group chat, you can send any information anonymously, allowing you to talk freely, leaving a small world for yourself."
+        },
+        SECURITY: {
+            TITLE: "Chat Anytime, Chat Anywhere",
+            SUBTITLE: "We will escort your information security all the time."
+        }
+    },
+
+    ABOUT_US: {
+        ABOUT_TITLE: {
+            TITLE: "Who We Are ?",
+            CONTENT: "We are an instant messaging team based in Hong Kong, China. In an era of big data, our personal information is facing great risks when we are enjoying great convenience brought by the internet. Our vision is to escort personal information security by resorting to technology. Support from you is indispensable to our future."
+        },
+        ABOUT_OPTIONS_ONE: {
+            TITLE: "Business Discussion",
+            CONTENT: "Business discussion and cooperation in any form are welcome to contact us by support@forward.im. Look forward to your mail!",
+            HREF: "聯繫我們"
+        },
+        ABOUT_OPTIONS_TWO: {
+            TITLE: "Customer Service",
+            CONTENT: "You can get available to customer service via the official account or emailing to  support@forward.im.",
+            HREF: "聯繫我們"
+        },
+        ABOUT_OPTIONS_THREE: {
+            TITLE: "Talent Recruitment",
+            CONTENT: "Talents from all over the world are welcome to join us and create a brilliant future together!",
+            HREF: "查看更多"
+        }
+    },
+
+    FOOTER: {
+
+    }
+};
---
+++ website/src/js/i18n.js
@@ -0,0 +1,94 @@
+﻿import zh from "../i18n/zh";
+import ct from "../i18n/ct";
+import en from "../i18n/en";
+
+export default class I18n {
+    constructor(lang) {
+        this.lang = lang || localStorage.language || navigator.language.split('-')[0];
+        this.resource = this.lang == "en" ? en : zh;
+    }
+
+    //Internationalizate whole page
+    fillPage() {
+        this.fill2($("[i18n]"));
+    }
+
+    //Internationalizate the children of this special 'JQuery element'
+    fillArea(element) {
+        //this.fill(element.find('[i18n]'));原来i18n方法
+        this.fill2(element.find("[i18n]"));
+        try {
+            Permission.check(element);
+        } catch (e) {
+            console.log("check permission failed:" + e);
+        }
+    }
+
+    fillAreaAttribute(element, attributeName) {
+        this.fill(element.find("[i18n][" + attributeName + "]"), attributeName);
+    }
+
+    fill(arrElement, attributeName) {
+        for (var i = 0, arrPath, text, len = arrElement.length; i < len; i++) {
+            arrPath = arrElement[i].attributes["i18n"].value.split(".");
+
+            text = this.resource;
+            for (var j = 0; j < arrPath.length; j++) {
+                text = text && text[arrPath[j]];
+            }
+
+            if (!attributeName) {
+                arrElement[i].innerHTML = text;
+            } else {
+                arrElement[i].setAttribute(attributeName, text);
+            }
+        }
+    }
+    getI18nValue(i18nKey) {
+        if (!i18nKey) {
+            return "";
+        }
+        var arrPath = i18nKey.split(".");
+
+        var text = this.resource;
+        for (var j = 0; j < arrPath.length; j++) {
+            text = text && text[arrPath[j]];
+        }
+        return text;
+    }
+
+    fill2(arrElement) {
+        for (var i = 0, len = arrElement.length; i < len; i++) {
+            var i18nValue = arrElement[i].attributes["i18n"];
+            var items = i18nValue.value.split(";"),
+                item,
+                attrMap;
+            for (var j = 0; j < items.length; j++) {
+                item = items[j];
+                if (!item) {
+                    continue;
+                }
+                if (item.indexOf("=") === -1) {
+                    i18nValue = this.getI18nValue(item);
+                    arrElement[i].innerHTML = i18nValue;
+                } else {
+                    attrMap = item.split("=");
+                    if (!attrMap[0]) {
+                        continue;
+                    }
+                    arrElement[i].setAttribute(attrMap[0], this.getI18nValue(attrMap[1]));
+                }
+            }
+        }
+    }
+
+    //params: strPath: ex, observerScreen.menu.NAV_OBSERVER_TITLE. return: value
+    findContent(strPath) {
+        var arrPath = strPath.split(".");
+        var text = this.resource;
+        for (var i = 0, len = arrPath.length; i < len; i++) {
+            text = text && text[arrPath[i]];
+        }
+        return text;
+    }
+}
